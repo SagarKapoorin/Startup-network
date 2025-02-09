797ac51ae9b5dbaf14fd159768c8a52690ffd9e8
Sagar Kapoor <142504626+SagarKapoorin@users.noreply.github.com>
2025-02-09 14:04:13 +0530

    Update route.ts

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -2,12 +2,12 @@ import { checkRechargeEmails } from "@/helper/recharge";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
-  const authHeader = request.headers.get("authorization");
-  if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
-    return new Response("Unauthorized", {
-      status: 401,
-    });
-  }
+  // const authHeader = request.headers.get("authorization");
+  // if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+  //   return new Response("Unauthorized", {
+  //     status: 401,
+  //   });
+  // }
   await checkRechargeEmails();
   console.log("Cron Job Ran at: ", new Date());
 
